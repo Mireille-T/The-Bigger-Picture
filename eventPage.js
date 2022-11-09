02426e327a5bfaa2ebab6fed9b6b0a7e2250f1e1
Mireille-T <69097387+Mireille-T@users.noreply.github.com>
2022-11-08 22:15:14 -0600

    Added context menu
    - eventPage.js: Added context menu and event listener to catch interaction
    - manifest.json: Added context menu permission

--- eventPage.js
+++ eventPage.js
@@ -1,2 +1,16 @@
 // Add JavaScript for processes that run in the background
 // Examples of code to be included here are: context menus, notifications, etc.
+
+var contextMenuItem = {
+    "id": "TheBiggerPicture",
+    "title": "Generate alt text",
+    "contexts": ["image"]
+}
+chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function (data) {
+    console.log(data);
+    if (data.menuItemId == "TheBiggerPicture") {
+        console.log("The Bigger Picture: Context menu item clicked!");
+    }
+})
